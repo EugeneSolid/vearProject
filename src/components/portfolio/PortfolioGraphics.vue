8ceb7f1de4f95a26e9bb1b68ec2b62bea3546273
dmitry-petrov <dmitry.p.biz@solid-sl.com>
2024-05-17 13:52:35 +0300

    Small fixes

--- src/components/portfolio/PortfolioGraphics.vue
+++ src/components/portfolio/PortfolioGraphics.vue
@@ -28,7 +28,7 @@ export default {
     </div>
   </div>
     <div class="position-absolute graphic-grade">
-      <div class="d-flex justify-content-between">
+      <div class="d-flex justify-content-between prices">
         <p class="mb-0 graphic-grade__value">$89.170</p>
         <p class="mb-0 graphic-grade__value">$114.203</p>
       </div>

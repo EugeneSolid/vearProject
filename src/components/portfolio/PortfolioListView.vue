303eb3b556eb58de05d4fcb5043e173b154c796f
dmitry-petrov <dmitry.p.biz@solid-sl.com>
2024-05-17 14:22:30 +0300

    Small margin fix

--- src/components/portfolio/PortfolioListView.vue
+++ src/components/portfolio/PortfolioListView.vue
@@ -8,7 +8,7 @@ export default {
 <div class="portfolio-list__list">
   <table class="w-100 default-table">
   <tr class="table-columns">
-      <th style="width: 190px">Token</th>
+      <th style="width: 195px">Token</th>
       <th style="width: 190px">Tokens owned</th>
       <th class="ps-2" style="width: 140px">Token Price</th>
       <th style="width: 160px">Tokens Value</th>
@@ -69,7 +69,7 @@ export default {
     </tr>
     <tr class="separator header"/>
     <tr class="table-row">
-      <td class="table-text bold"><img class="table-property-img" src="/vearProject/images/properties/property-1.png">LE0001</td>
+      <td class="table-text bold"><img class="table-property-img property" src="/vearProject/images/properties/property-1.png">LE0001</td>
       <td style="height: 96px" class="pe-0 table-data d-flex align-items-center justify-content-between">
         3.000
         <span class="currency-changes up gap-1"><i class="fi fi-sr-caret-up"></i>28%</span>
@@ -84,7 +84,7 @@ export default {
     </tr>
     <tr class="separator header"/>
     <tr class="table-row">
-      <td class="table-text bold"><img class="table-property-img" src="/vearProject/images/properties/property-2.png">LE0002</td>
+      <td class="table-text bold"><img class="table-property-img property" src="/vearProject/images/properties/property-2.png">LE0002</td>
       <td style="height: 96px" class="pe-0 table-data d-flex align-items-center justify-content-between">
         3.000
         <span class="currency-changes down gap-1"><i class="fi fi-sr-caret-down"></i>-14.5%</span>
@@ -99,7 +99,7 @@ export default {
     </tr>
     <tr class="separator header"/>
     <tr class="table-row">
-      <td class="table-text bold"><img class="table-property-img" src="/vearProject/images/properties/property-3.png">LE0003</td>
+      <td class="table-text bold"><img class="table-property-img property" src="/vearProject/images/properties/property-3.png">LE0003</td>
       <td class="table-data">3.000</td>
       <td class="table-data ps-2">$1</td>
       <td class="table-data">$3.000</td>
